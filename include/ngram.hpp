0d262fca7be4009cc9cd17767fd99379f8e96183
JangYoungWhan <stdjangyoungwhan@gmail.com>
2015-09-08 23:47:23 +0900

    Implement general purpose template classes.

--- include/ngram.hpp
+++ include/ngram.hpp
@@ -1,5 +1,5 @@
 /*
- *  File    : ngramvector.hpp
+ *  File    : ngram.hpp
  *  Author  : YW. Jang
  *  Date    : 2015.08.28.Fri
  *
@@ -15,7 +15,7 @@
 
 namespace nlp { namespace jang { namespace garnut {
 
-template <typename T_key>
+template <typename T_StrType>
 class Ngram
 {
 public:
@@ -24,7 +24,13 @@ public:
 
   }
 
-  Ngram(const std::vector<T_key>& ngram)
+  Ngram(unsigned int n)
+    :n_(n)
+  {
+    ngram_.reserve(n);
+  }
+
+  Ngram(const std::vector<T_StrType>& ngram)
   {
     this->ngram_ = ngram;
   }
@@ -35,9 +41,63 @@ public:
   }
 
 public:
+  void push_back(const T_StrType& token)
+  {
+    ngram_.push_back(token);
+  }
+
+public:
+  unsigned int getN() const
+  {
+    return this->n_;
+  }
+
+public: // operator overloading
+  // Non-const variants
+  T_StrType& operator[](std::size_t idx)
+  {
+    return ngram_[idx];
+  }
+
+  // Const variants
+  const T_StrType& operator[](std::size_t idx) const
+  {
+    return ngram_[idx];
+  }
+
+  bool operator==(const Ngram& rhs) const
+  {
+    return this->ngram_ == rhs.ngram_;
+  }
+
+  bool operator!=(const Ngram& rhs)
+  {
+    return !(this->ngram_ == rhs);
+  }
+
+  bool operator>(const Ngram& rhs)
+  {
+    return this->ngram_ > rhs.ngram_;
+  }
+
+  bool operator>=(const Ngram& rhs)
+  {
+    return  this->ngram_ >= rhs.ngram_;
+  }
+  
+  bool operator<(const Ngram& rhs)
+  {
+    return  this->ngram_ < rhs.ngram_;
+  }
+  
+  bool operator<=(const Ngram& rhs)
+  {
+    return  this->ngram_ <= rhs.ngram_;
+  }
 
 protected:
-  std::vector<T_key> ngram_;
+  std::vector<T_StrType> ngram_;
+  unsigned int n_;
 };
 
 } } } // nlp::jang::garnut
